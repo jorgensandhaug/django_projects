c968664fd899ba22323617aca070cddbed049faf
jorgensandhaug <55702375+jorgensandhaug@users.noreply.github.com>
2021-01-31 19:39:08 +0100

    la til comments

--- blog/blogs/models.py
+++ blog/blogs/models.py
@@ -5,7 +5,6 @@ from django.utils import timezone
 class User(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
-
     def __str__(self):
         return self.name
     
@@ -24,11 +23,7 @@ class Blogpost(models.Model):
 
 
 class Comment(models.Model):
-    title = models.CharField(max_length=255)
     content = models.CharField(max_length=1000)
     publisher = models.ForeignKey(User, on_delete=models.CASCADE)
     blogpost = models.ForeignKey(Blogpost, on_delete=models.CASCADE)
-    
-    def __str__(self):
-        return self.title
     
